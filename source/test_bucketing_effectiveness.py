99cf57ffc167eac247f84fbaca47a37c8d2152ef
Christian Harjuno <harjuno.tian@gmail.com>
2025-08-25 00:52:18 +0900

    final commit on experiment size

---
+++ source/test_bucketing_effectiveness.py
@@ -0,0 +1,72 @@
+import numpy as np
+from torch import i0
+from load_dataset import process_dataset
+from create_buckets import create_buckets
+from build_dataset import build_dataset_bucketed, calculate_label_ratio
+from tokenizer_functions import get_tokens
+from train_model import train
+import warnings
+from tokenizer_functions import get_tokens
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
+# first import and the dataset from huggingface
+
+print("Loading and processing dataset...")
+training_dataset_reduced, test_dataset_reduced = process_dataset(split_size=0.01)
+print(f"Dataset loaded and processed. Loaded {len(training_dataset_reduced)} training samples and {len(test_dataset_reduced)} test samples.")
+
+print("Loading embeddings...")
+embeddings = np.load("bert_mean_pooled_embeddings.npy")
+print("Loaded embeddings with shape:", embeddings.shape)
+
+# Start bucketing
+print("Starting bucketing process...")
+training_dataset_reduced, unique_labels, labels = create_buckets(training_dataset_reduced, embeddings, min_cluster_size=200)
+
+positive_ratio, negative_ratio = calculate_label_ratio(training_dataset_reduced, "label")
+
+grouped_datasets = [
+  training_dataset_reduced.select(indices) for label in unique_labels for indices in [np.where(labels == label)[0]]
+]
+
+print(f"Created {len(grouped_datasets)} buckets based on HDBSCAN clustering.")
+
+for ds in grouped_datasets:
+  print(f"Bucket size: {len(ds)}")
+  
+# create a prompt to select training index
+
+training_ds = grouped_datasets[2 + 1]
+
+training_ds = training_ds.map(get_tokens, batched=True, batch_size=8)
+training_ds = training_ds.rename_column("label", "labels")
+
+training_ds_split = training_ds.train_test_split(test_size=0.15, seed=42)
+
+training_train_ds = training_ds_split["train"]
+training_eval_ds = training_ds_split["test"]
+
+training_train_ds_split = training_train_ds.train_test_split(test_size=0.1, seed=42)
+
+train(training_train_ds_split["train"], training_train_ds_split["test"], training_eval_ds, "Bucket 2 to 2")
+
+# for i in range(2100, training_dataset_reduced.num_rows, 100):
+#   print(f"Processing {i} samples...")
+#   final_train_dataset, final_test_dataset = build_dataset_bucketed(grouped_datasets, i, positive_ratio, negative_ratio)
+
+#   print(f"Final dataset built. Training set size: {len(final_train_dataset)}, Test set size: {len(final_test_dataset)}")
+
+#   final_train_dataset = final_train_dataset.map(get_tokens, batched=True, batch_size=8)
+#   final_test_dataset = final_test_dataset.map(get_tokens, batched=True, batch_size=8)
+#   test_dataset_reduced = test_dataset_reduced.map(get_tokens, batched=True, batch_size=8)
+
+#   if "labels" not in final_train_dataset.column_names:
+#     final_train_dataset = final_train_dataset.rename_column("label", "labels")
+#   if "labels" not in final_test_dataset.column_names:
+#     final_test_dataset = final_test_dataset.rename_column("label", "labels")
+#   if "labels" not in test_dataset_reduced.column_names:
+#     test_dataset_reduced = test_dataset_reduced.rename_column("label", "labels")
+#   print("Tokenization complete. Starting training...")
+
+#   train(final_train_dataset, final_test_dataset, test_dataset_reduced, "Bucketing")
